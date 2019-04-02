e7c3b3691b21b1f0d0904d602a933a37e100ba63
alwyn <gwwen@cn.ibm.com>
2019-04-02 15:20:43 +0800

    implement table simply bby mat-table

--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 A sample to implement table-tree by mat-table and dynamic drawing charts with d3 
 
 
-#### branch list
+### view different code on those branches
 ###### step0:
 
 initial angular and ready data
+
+###### step1:
+
+simple mat-table implement
+
+key point: 
+
+  - "ng-container" define column
+  - "tr" define row
+  - "renderRows" refresh page
+  
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,31 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTable } from '@angular/material';
 import { DataSource } from '@angular/cdk/collections';
-import { animate, state, style, transition, trigger } from '@angular/animations';
 import { element } from 'protractor';
 import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.sass']
+  styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
+  @ViewChild(MatTable) table: MatTable<any>;
 
-  private dataSource: any[] = [];
-  private dataHeader: any[] = [];
+  dataSource: any[] = [];
+  dataHeader: any[] = [];
+  headers: string[] = ['name'];
 
   constructor(private post: PostService) {
 
     for (let i = 1; i < 13; i++) {
         this.dataHeader.push(i + "月");
     }
+    this.headers.push(...this.dataHeader);
+    console.log(this.headers);
 
     post.readData()
     .subscribe( res => {
-        console.log(res);
+        for (let r of res) {
+            let row = {'name': r['name']};
+            for (let j = 0; j < this.dataHeader.length; j++) {
+                row[this.dataHeader[j]] = r['scores'][j]['total'];
+            }
+            this.dataSource.push(row);
+        }
+        console.log(this.dataSource);
+        this.table.renderRows();
     })
-
   }
 
   ngOnInit() {
